1703f1e362ea4d61096d8c5445d8a6f6987a2fb4
vikshit <vikshit@localhost>
2024-04-20 19:19:40 +0530

    commit my changes to local git repo

--- src/test/java/com/gitpractice/testbase/DemoTest.java
+++ src/test/java/com/gitpractice/testbase/DemoTest.java
@@ -4,7 +4,15 @@ public class DemoTest {
 
 	public static void main(String[] args) {
 		System.out.println("Test");
-
+		
+		int age =15;
+		if(age>=18) {
+			System.out.println("Cast the vote");
+	
+		}
+		else {
+			System.out.println("Cant Cast the vote");
+		}
 	}
 
 }
