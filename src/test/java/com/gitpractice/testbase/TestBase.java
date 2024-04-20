b9aa863fb7dd1250f73862832716500a3a005df1
vikshit <vikshit@localhost>
2024-04-20 23:33:30 +0530

    test base const updating

--- src/test/java/com/gitpractice/testbase/TestBase.java
+++ src/test/java/com/gitpractice/testbase/TestBase.java
@@ -2,9 +2,16 @@ package com.gitpractice.testbase;
 
 public class TestBase {
   
+	{
+		System.out.println("TestBase Const");
+		
+	}
   public static void main(String[] args){
 
     System.out.println("Hello team");
+    
+    new TestBase();
+    
   }
 
 }
